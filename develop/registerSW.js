1505204b3700d1b953625929c01bd51359687127
koichii <koichii@users.noreply.github.com>
2026-06-11 04:34:09 +0000

    deploy: a9e1feffc868dde64f52444115a5c752d5f61854

--- develop/registerSW.js
+++ develop/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/develop/sw.js', { scope: '/develop/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
